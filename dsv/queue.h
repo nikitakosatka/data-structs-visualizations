cdd573c34f9f4f18b3e338ebc7ecf0d712074867
nikitakosatka <nikitakosatka@yandex.ru>
2020-04-11 20:39:09 +0500

    Переход в начале экрана очереди

--- dsv/queue.h
+++ dsv/queue.h
@@ -11,9 +11,13 @@ private:
     
     Texture bgTexture;
     Texture textTexture;
+    Texture shadingTexture;
     
     Animation backgroundAnimation;
     AnimatedSprite background;
+    
+    Animation shadingAnimation;
+    AnimatedSprite shading;
     
     Sprite text;
     
@@ -28,6 +32,8 @@ public:
     }
     
     void run() {
+        start();
+        
         while (window->isOpen()) {
             events();
             update();
@@ -66,6 +72,7 @@ public:
     }
     
     void loadTextures() {
+        shadingTexture.loadFromFile(resourcePath() + "shading.png");
         bgTexture.loadFromFile(resourcePath() + "queue_background.png");
         textTexture.loadFromFile(resourcePath() + "queue_text.png");
     }
@@ -78,8 +85,36 @@ public:
         
         background = AnimatedSprite(seconds(0.1), true, true);
         
+        shadingAnimation.setSpriteSheet(shadingTexture);
+        for (int frame = 0; frame < 5; frame++) {
+            shadingAnimation.addFrame(IntRect(0, frame * HEIGHT, WIDTH, HEIGHT));
+        }
+        
+        shading = AnimatedSprite(seconds(0.1), true, false);
+        
         text = Sprite(textTexture);
         text.setPosition((WIDTH - text.getTextureRect().width) / 2, 50);
+    }
+    
+    void start() {
+        for (int frame = 0; frame < 30; frame++) {
+            events();
+            window->clear();
+            
+            Time frameTime = frameClock.restart();
+            
+            background.play(backgroundAnimation);
+            background.update(frameTime);
+            
+            shading.play(shadingAnimation);
+            shading.update(frameTime);
+            
+            window->draw(background);
+            window->draw(text);
+            window->draw(shading);
+            
+            window->display();
+        }
     }
 };
 
