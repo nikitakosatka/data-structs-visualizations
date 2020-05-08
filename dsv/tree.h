6dc9b4e48cddd5e94436e979656232a3a8fee7e5
nikitakosatka <nikitakosatka@yandex.ru>
2020-05-09 01:30:12 +0500

    Заготовка дерева

---
+++ dsv/tree.h
@@ -0,0 +1,32 @@
+#ifndef tree_h
+#define tree_h
+
+#include "utils.h"
+
+class Tree {
+private:
+    RenderWindow *window;
+    
+public:
+    Tree(RenderWindow *window) {
+        this->window = window;
+    }
+    
+    Tree() {
+        // Default constructor
+    }
+    
+    void run() {
+        
+    }
+    
+    void events() {
+        
+    }
+    
+    void update() {
+        
+    }
+};
+
+#endif /* tree_h */
